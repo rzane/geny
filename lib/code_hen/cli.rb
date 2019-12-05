40583ec4e3cea2ce7c37e53f9e65cdc33c6e0b6c
Ray Zane <raymondzane@gmail.com>
2019-12-05 02:38:51 -0500

    Extract logic for assembling a command

--- lib/code_hen/cli.rb
+++ lib/code_hen/cli.rb
@@ -1,60 +1,29 @@
 require "code_hen"
-require "code_hen/dsl"
 require "code_hen/parser"
-require "code_hen/generator"
+require "code_hen/command"
+require "code_hen/version"
 
 module CodeHen
   class CLI
     MANIFEST = "generator.rb"
-    ROOTS = [File.expand_path("../generators", __dir__)]
+    LOAD_PATH = [File.expand_path("../generators", __dir__)]
 
-    def self.run(*args)
-      new(*args).run
-    end
-
-    def initialize(roots: ROOTS, argv: ARGV, version: CodeHen::VERSION)
-      @argv = argv
-      @roots = roots
+    def initialize(version: CodeHen::VERSION, load_path: LOAD_PATH)
       @version = version
+      @load_path = load_path
     end
 
-    def run
+    def run(argv)
       parser = Parser.new
       parser.version @version
       parser.argument :command, desc: "command to run", required: true
 
-      options = parser.parse(@argv)
-      command_name = options[:command]
-      command_args = options[:unused_arguments]
-      run_subcommand(command_name, command_args)
-    end
+      options = parser.parse(argv)
+      name = options[:command]
+      args = options[:unused_arguments]
 
-    def run_subcommand(name, argv)
-      manifest = find_manifest(name)
-
-      dsl = DSL.new
-      dsl.instance_eval File.read(manifest)
-
-      parser = Parser.new
-      parser.version @version
-      parser.instance_eval(&dsl.parse)
-
-      context = parser.parse(argv)
-      generator = Generator.new(context: context)
-      dsl.helpers.each { |h| generator.extend(h) }
-      generator.instance_eval(&dsl.invoke)
-    end
-
-    private
-
-    def find_manifest(command)
-      path = command.split(":")
-      paths = @roots.map { |root| File.join(root, *path, MANIFEST) }
-      paths.find { |m| File.exist?(m) } || invalid_command!(command)
-    end
-
-    def invalid_command!(command)
-      raise Error, "There doesn't appear to be a generator named '#{command}'."
+      command = Command.new(name: name, load_path: @load_path)
+      command.run(args)
     end
   end
 end
---
+++ lib/code_hen/command.rb
@@ -0,0 +1,58 @@
+require "code_hen/dsl"
+require "code_hen/parser"
+require "code_hen/generator"
+
+module CodeHen
+  class Command
+    attr_reader :name, :load_path
+
+    def initialize(name:, load_path:)
+      @name = name
+      @dsl = DSL.new
+      @parser = Parser.new
+      @loaded = false
+      @load_path = load_path
+    end
+
+    def run(argv)
+      load! unless loaded?
+      invoke parser.parse(argv)
+    end
+
+    def invoke(context)
+      load! unless loaded?
+      generator = Generator.new(context: context)
+      dsl.helpers.each { |h| generator.extend(h) }
+      generator.instance_eval(&dsl.invoke)
+    end
+
+    private
+
+    attr_reader :dsl, :parser, :load_path
+
+    def loaded?
+      @loaded
+    end
+
+    def load!
+      @loaded = load_path.any? { |path|
+        file = File.join(path, *name.split(":"), "generator.rb")
+        data = begin
+                 File.read(file)
+               rescue Errno::ENOENT
+                 next false
+               end
+
+        dsl.instance_eval(data)
+        parser.instance_eval(&dsl.parse)
+        true
+      }
+
+      command_not_found! unless loaded?
+    end
+
+    def command_not_found!
+      raise Error, "There doesn't appear to be a generator named '#{name}'."
+    end
+  end
+end
