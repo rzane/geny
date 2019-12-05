7581907ea11e13f2952d1cc1bc2ee33ccfd5b6b0
Ray Zane <raymondzane@gmail.com>
2019-12-04 20:05:42 -0500

    Start parsing!

--- lib/code_hen.rb
+++ lib/code_hen.rb
@@ -2,5 +2,48 @@ require "code_hen/version"
 
 module CodeHen
   class Error < StandardError; end
-  # Your code goes here...
+
+  class CLI
+    def initialize(argv: ARGV)
+      @argv = ARGV
+    end
+
+    def run
+    end
+  end
+
+  class DSL
+    attr_reader :options, :arguments, :helpers
+
+    def initialize
+      @arguments = []
+      @options = []
+      @helpers = []
+      @parse = ->(opts){ opts }
+      @generate = -> { warn "I don't know what to do!" }
+    end
+
+    def argument(name)
+      @arguments << name
+    end
+
+    def option(name)
+      @options << name
+    end
+
+    def parse(&block)
+      @parse = block if block_given?
+      @parse
+    end
+
+    def generate(&block)
+      @generate = block if block_given?
+      @generate
+    end
+
+    def helpers(&block)
+      @helpers << Module.new(&block) if block_given?
+      @helpers
+    end
+  end
 end
--- spec/code_hen_spec.rb
+++ spec/code_hen_spec.rb
@@ -3,7 +3,41 @@ RSpec.describe CodeHen do
     expect(CodeHen::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe CodeHen::DSL do
+    subject(:dsl) { described_class.new }
+
+    it "allows defining arguments" do
+      dsl.argument :foo
+      expect(dsl.arguments).to eq([:foo])
+    end
+
+    it "allows defining options" do
+      dsl.option :foo
+      expect(dsl.options).to eq([:foo])
+    end
+
+    it "has a default parser" do
+      expect(dsl.parse.call(value: 1)).to eq(value: 1)
+    end
+
+    it "allows defining parse behavior" do
+      dsl.parse do |value:|
+        {value: value + 1}
+      end
+
+      expect(dsl.parse.call(value: 1)).to eq(value: 2)
+    end
+
+    it "has a default generator" do
+      expect { dsl.generate.call }.to output("I don't know what to do!\n").to_stderr
+    end
+
+    it "allows defining generate behavior" do
+      dsl.generate do
+        puts "hit!"
+      end
+
+      expect { dsl.generate.call }.to output("hit!\n").to_stdout
+    end
   end
 end
