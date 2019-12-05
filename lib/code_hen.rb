4fd2938a8bc4c7db4a16830ea75447c2add1d43f
Ray Zane <raymondzane@gmail.com>
2019-12-04 19:19:50 -0500

    First commit

---
+++ lib/code_hen.rb
@@ -0,0 +1,6 @@
+require "code_hen/version"
+
+module CodeHen
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/code_hen_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe CodeHen do
+  it "has a version number" do
+    expect(CodeHen::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
