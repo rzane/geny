c4c8867a94bb621f62f60b5fb46fef8c4fa28a16
Ray Zane <raymondzane@gmail.com>
2019-12-06 14:46:40 -0500

    Better help generation

--- lib/geny/cli.rb
+++ lib/geny/cli.rb
@@ -5,40 +5,50 @@ require "geny/version"
 
 module Geny
   class CLI
-    attr_reader :version, :registry
+    attr_reader :registry, :version, :program_name, :description
 
-    def initialize(version: VERSION, registry: Registry.new)
-      @version = version
+    def initialize(
+      registry: Registry.new,
+      version: VERSION,
+      program_name: "geny",
+      description: nil
+    )
       @registry = registry
+      @version = version
+      @program_name = program_name
+      @description = description
     end
 
     def run(argv)
-      name, *args = argv
+      command_name, *command_args = argv
 
-      case name
+      case command_name
       when nil, '-h', '--help'
         print_help
       when '-v', '--version'
         ui.say version
       else
-        command = registry.find!(name)
-        command.run(args)
+        command = registry.find!(command_name)
+        command.run(command_args)
       end
     end
 
     private
 
     def print_help
-      ui.say ui.color.bold("USAGE")
-      ui.say "  geny [COMMAND]"
-      ui.say ui.color.bold("\nARGUMENTS")
-      ui.say "  COMMAND              command to run (required)"
-      ui.say ui.color.bold("\nFLAGS")
-      ui.say "  -v, --version        print version and exit"
-      ui.say "  -h, --help           show this help and exit"
+      parser = Argy.new do |o|
+        o.usage("#{program_name} [COMMAND]")
+        o.description(description)
+        o.argument :command, desc: "generator to run", required: true
+        o.on "-v", "--version", "print version and exit"
+      end
+
+      ui.say parser.help(column: 20)
       ui.say ui.color.bold("\nCOMMANDS")
-      registry.scan.each do |command|
-        ui.say "  #{command.name}#{command.description&.rjust(40)}"
+
+      registry.scan.each do |cmd|
+        desc = ui.color.dim(cmd.description || "")
+        ui.say "  #{cmd.name.ljust(20)}#{desc}"
       end
     end
 
